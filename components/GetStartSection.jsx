b86b107817b0d26894f7d074350177847f606612
Maxwell Shick <maxwellshick@gmail.com>
2022-07-16 16:56:40 -0400

    Deploying

--- components/GetStartSection.jsx
+++ components/GetStartSection.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Link from 'next/link';
 
 const GetStartSection = () => {
   return (
-    <section id='getStartedSection'>
-      <h3 className='h3Head secondaryHeadingHighlight'>Let's Get Started</h3>
+    <section id='getStartedSection' className='flexColCenter'>
+      <h3 className='h3Head blackHeading shrinkHeadingPandM spaceMonoFont'>LET'S GET STARTED</h3>
+      <h2 className='shrinkHeadingPandM blackHeading h2Head'>Leave the busy work to us.</h2>
+
+      <div id='getStartedContainer'>
+        <h4 className='h4Head blackHeading'>Try our tool in beta</h4>
+        <Link href='/getting_started/'>
+          <button className='secondaryButton'>Get Started</button>
+        </Link>
+      </div>
+
     </section>
   )
 }
